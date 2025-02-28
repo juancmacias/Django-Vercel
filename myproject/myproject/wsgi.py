4b3300d44132bf18b205f524fd9e47ff1dfee314
juan carlos macias <juancmaciassalvador@gmail.com>
2025-02-28 12:26:54 +0100

    casi todo

--- myproject/myproject/wsgi.py
+++ myproject/myproject/wsgi.py
@@ -12,6 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 #os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django-vercel-beryl.vercel.app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'versel_deploymet.settings')
 application = get_wsgi_application()
+app = get_wsgi_application()
 
